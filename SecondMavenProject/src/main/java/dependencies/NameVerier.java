6cf1c887178f1ddd3faf4f11e4794cca8af703da
Evelyn <totalnieniewybaczalne.ep@gmail.com>
2019-02-13 20:37:03 +0100

    first test

--- SecondMavenProject/src/main/java/dependencies/NameVerier.java
+++ SecondMavenProject/src/main/java/dependencies/NameVerier.java
@@ -2,12 +2,15 @@ package dependencies;
 
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NameVerier {
-//    public static WebDriver driver;
-//    public static final String BASE_URL = "https://javastart.pl/kurs/selenium";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.lang.IllegalStateException {
         String name = "Maciek";
         System.out.println(StringUtils.isAlpha(name));
-      //  driver.get(BASE_URL);
+        System.setProperty("webdriver.chrome.driver", "D:\\JAVA-Projects\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://allegro.pl/");
+
     } }
