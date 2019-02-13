e23c9baafc93c7d74646e75cb340a02a96515528
Ewelina Pałka <totalnieniewybaczalne.ep@gmail.com>
2019-02-13 18:29:46 +0100

    testing class and others files

---
+++ SecondMavenProject/src/main/java/dependencies/NameVerier.java
@@ -0,0 +1,8 @@
+package dependencies;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class NameVerier {
+    public static void main(String[] args) {
+        String name = "Maciek";
+        System.out.println(StringUtils.isAlpha(name));  } }
