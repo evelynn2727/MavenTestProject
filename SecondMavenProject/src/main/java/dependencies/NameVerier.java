6ad71ab260a31c15d3e88c8fbe9d96d29d7258c0
Evelyn <totalnieniewybaczalne.ep@gmail.com>
2019-02-13 19:18:13 +0100

    rebuild with selenium dependency

--- SecondMavenProject/src/main/java/dependencies/NameVerier.java
+++ SecondMavenProject/src/main/java/dependencies/NameVerier.java
@@ -1,8 +1,13 @@
 package dependencies;
 
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.WebDriver;
 
 public class NameVerier {
+//    public static WebDriver driver;
+//    public static final String BASE_URL = "https://javastart.pl/kurs/selenium";
     public static void main(String[] args) {
         String name = "Maciek";
-        System.out.println(StringUtils.isAlpha(name));  } }
+        System.out.println(StringUtils.isAlpha(name));
+      //  driver.get(BASE_URL);
+    } }
